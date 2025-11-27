0bf21a46ffce703eb1a0661d3b011dd03615017c
bijoymandal <bijoy.mandal@mydevteam.com>
2025-11-27 12:58:55 +0530

    Message bottom component

---
+++ lib/screens/pages/messages_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MessagesPage extends StatelessWidget {
+  const MessagesPage({super.key});
+  @override
+  Widget build(BuildContext context) => const Center(
+    child: Text(
+      "Messages",
+      style: TextStyle(fontSize: 32, color: Colors.green),
+    ),
+  );
+}
